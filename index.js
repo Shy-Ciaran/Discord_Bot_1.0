c3ea9c825e61e04d4f844a5b21630e4a72606bb1
Shy-Ciaran <ichigo53@live.com>
2021-04-08 15:32:55 -0500

    sample message

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 //code here under here blah
 const Discord = require('discord.js')
+const { prefix, token } = require('./config.json')
+
 const bot = new Discord.Client();
 
-bot.login("ODI5NDQ0MTIxMjk0OTI5OTIx.YG4OCA.jiJWci35llfakTlOIscsG9tNZls")
+bot.login(token);
+
+bot.on('message', message => {
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong.');
+    }
+    if(message.content === `${prefix}are you here?`){
+        message.channel.send('I am here')
+    }
+});
