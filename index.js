6c6f6d5e17df3591878166f63b5effdd4f9550d4
Brittany-Hill <bh41@students.uwf.edu>
2021-05-06 11:49:37 -0500

    added things

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 //code here under here blah
-const Discord = require('discord.js')
-const { prefix, token } = require('./config.json')
+require('dotenv').config()
+const Discord = require('discord.js');
+const { prefix } = require('./config.json'); 
 
 const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
 
-bot.login(token);
-
-bot.on('message', message =>{
-   if(message.content == `${prefix}DM`){
-   member.send("I am a sample message");
-   }
-});
-//bot.on('guildMemberAdd', member => {
-//   member.send("Welcome " + member.displayName + " to " + member.guild.name);
-//});
+bot.login(TOKEN);
 
 bot.on('message', message => {
-	if (message.content === `${prefix}ping`) {
-		message.channel.send('Pong.');
+    if (message.content == `${prefix}DM`) {
+        member.send("I am a sample message");
     }
-    if(message.content === `${prefix}are you here?`){
-        message.channel.send('I am here')
+});
+bot.on('guildMemberAdd', member => {
+    member.send("Welcome " + member.displayName + " to " + member.guild.name);
+});
+
+bot.on('message', message => {
+    if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong.');
+    }
+    if (message.content === `${prefix}are you here?`) {
+        message.channel.send('I am here');
     }
 });
